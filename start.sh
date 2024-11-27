354d1f7b85df398f808a3b2e85eb13bc5e1d9e2e
capitaineflam25 <985702+capitaineflam25@users.noreply.github.com>
2024-11-27 07:23:37 +0100

    Docker "dev" port 3000
    docker build -t planka -f Dockerfile.3000 .
    
    # version de test "port 3000"
    docker-compose -f docker-compose-3000.yml up

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#export NODE_ENV=production && set -e && node db/init.js && node app.js --prod
-export NODE_ENV=development && set -e && node db/init.js && node app.js
+export NODE_ENV=production && set -e && node db/init.js && node app.js --prod
+#export NODE_ENV=development && set -e && node db/init.js && node app.js
