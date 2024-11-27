c4a93fbb406106fb99fadb861285a4abcc289d79
capitaineflam25 <985702+capitaineflam25@users.noreply.github.com>
2024-11-27 08:55:11 +0100

    Dev

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #export NODE_ENV=production && set -e && node db/init.js && node app.js --prod
 # dev mode
-export NODE_ENV=development && set -e && node db/init.js && node app.js
+export NODE_ENV=development && set -e && node db/init.js && node app.js --dev
